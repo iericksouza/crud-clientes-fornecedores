602341286ab6ba49bfd19f326f753d47d8d131c0
iericksouza <erick.souza17@fatec.sp.gov.br>
2023-05-26 18:34:43 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ClientComponent } from './client/client.component';
+import { SupplierComponent } from './supplier/supplier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ClientComponent,
+    SupplierComponent
   ],
   imports: [
     BrowserModule,
